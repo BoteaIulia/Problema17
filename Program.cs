b3750d897ecf3c427e7699602b931686bdc8874e
botea <botea@IULIA>
2022-11-30 13:12:46 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+public static class Globals
+{
+
+	internal static void Main()
+	{
+
+		Console.Write("Determinati CMMDC si CMMMC a celor doua numere:");
+		Console.Write("\n");
+
+		int n;
+		int m;
+		int r;
+		int P;
+		Console.Write("Primul numar= ");
+		n = Convert.ToInt32(Console.ReadLine());
+		Console.Write("Al doilea  numar= ");
+		m = Convert.ToInt32(Console.ReadLine()); ;
+        P = n * m;
+
+
+		while (m != 0)
+		{
+
+
+		    r = n % m;
+			n = m;
+			m = r;
+
+
+		}
+		Console.Write("CMMDC= ");
+		Console.Write(n);
+		const char V = '\n';
+		Console.Write(V);
+		Console.Write("CMMMC= ");
+        int r1 = 'r';
+        Console.Write(value: P / r1);
+
+
+	}
+}
